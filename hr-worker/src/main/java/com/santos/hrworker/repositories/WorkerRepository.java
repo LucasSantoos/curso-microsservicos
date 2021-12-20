84a667229c9f30afdb9ca952fb691ca7d4c7c77a
LucasSantoos <lucas.ss@hotmail.com>
2021-12-20 20:33:20 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/santos/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.santos.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.santos.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
