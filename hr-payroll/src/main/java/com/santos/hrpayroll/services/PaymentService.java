7302756b76f96e07d7e7ff01f00a570d0fc9b833
LucasSantoos <lucas.ss@hotmail.com>
2021-12-20 23:38:23 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/santos/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.santos.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.santos.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
