a58d974c9350afeb1749aa079751951ba4841c24
LucasSantoos <lucas.ss@hotmail.com>
2022-01-04 18:16:49 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/santos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/santos/hrworker/resources/WorkerResource.java
@@ -1,21 +1,27 @@
 package com.santos.hrworker.resources;
 
-import java.util.List;
-
+import com.santos.hrworker.entities.Worker;
+import com.santos.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.santos.hrworker.entities.Worker;
-import com.santos.hrworker.repositories.WorkerRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Autowired
+	private Environment environment;
 
 	@Autowired
 	private WorkerRepository repository;
@@ -28,6 +34,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable(name = "id") Long id) {
+		logger.info("PORT = " + environment.getProperty("local.server.port"));
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
